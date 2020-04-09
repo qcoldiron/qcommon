0f46eb7a22c0543872fb38f89e002dd0adec58f2
qcoldiron <32491158+qcoldiron@users.noreply.github.com>
2020-04-09 18:39:14 -0500

    Create slack.go

---
+++ slack.go
@@ -0,0 +1,37 @@
+package slack
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+	"errors"
+	"bytes"
+)
+
+type SlackRequestBody struct {
+    Text string `json:"text"`
+}
+
+func SendSlackNotification(webhookUrl string, msg string) error {
+
+    slackBody, _ := json.Marshal(SlackRequestBody{Text: msg})
+    req, err := http.NewRequest(http.MethodPost, webhookUrl, bytes.NewBuffer(slackBody))
+    if err != nil {
+        return err
+    }
+
+    req.Header.Add("Content-Type", "application/json")
+
+    client := &http.Client{Timeout: 10 * time.Second}
+    resp, err := client.Do(req)
+    if err != nil {
+        return err
+    }
+
+    buf := new(bytes.Buffer)
+    buf.ReadFrom(resp.Body)
+    if buf.String() != "ok" {
+        return errors.New("Non-ok response returned from Slack")
+    }
+    return nil
+}
